11b0b613357974f3807766f15935a6399ecd3ee5
appadilokesh04-source <appadilokesh04@gmail.com>
2025-12-16 16:09:58 +0530

    Delete final project directory

--- final project/app/routes/__init__.py
+++
@@ -1,3 +0,0 @@
-# app/routes/__init__.py
-from . import users, products, categories, orders  # noqa: F401
-
--- final project/app/schemas/__init__.py
+++
@@ -1,4 +0,0 @@
-from .schemas import UserCreate, UserOut, Token, CategoryCreate, CategoryOut, ProductCreate, ProductOut, OrderItemIn, OrderCreate, OrderItemOut, OrderOut
-# (Or whatever classes you need from schemas.py)
-
-
